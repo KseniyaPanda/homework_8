5e36f6a40f300e604059925edf5cf4d9d53ab48d
KseniyaPanda <ksenia-22-11@mail.ru>
2022-12-20 23:38:29 +0300

    056

--- homework056/Program.cs
+++ homework056/Program.cs
@@ -10,3 +10,83 @@
 с наименьшей суммой элементов: 1 строка
 */
 
+// получить числа с консоли
+int GetNumber(string message)
+{
+   int result = 0;
+   while (true)
+   {
+      Console.WriteLine(message);
+      if (int.TryParse(Console.ReadLine(), out result) && result > 0)
+      {
+         break;
+      }
+      else
+      {
+         Console.WriteLine("Ввели не число");
+      }
+   }
+   return result;
+}
+
+// задать матрицу и заполнить ее числами
+int[,] GetMatrix(int m, int n)
+{
+   int[,] matrix = new int[m, n];
+   Random rnd = new Random();
+   for (int i = 0; i < matrix.GetLength(0); i++)
+   {
+      for (int j = 0; j < matrix.GetLength(1); j++)
+      {
+         matrix[i, j] = rnd.Next(1, 10);
+      }
+   }
+   return matrix;
+}
+
+// распечатать матрицу
+void PrintMatrix(int[,] matrix)
+{
+   Console.WriteLine();
+   for (int i = 0; i < matrix.GetLength(0); i++)
+   {
+      for (int j = 0; j < matrix.GetLength(1); j++)
+      {
+         Console.Write($"{matrix[i, j]} ");
+      }
+      Console.WriteLine();
+   }
+}
+
+void MinSum(int[,] matrix)
+{
+   int minNum = 0;
+   int minSum = 0;
+   for (int i = 0; i < matrix.GetLength(0); i++)
+   {
+      int sum = 0;
+      for (int j = 0; j < matrix.GetLength(1); j++)
+      {
+         sum = sum + matrix[i, j];
+      }
+      //Console.WriteLine($"Сумм {i + 1} стр {sum}\n");
+      if (i == 0)
+      {
+         minSum = sum;
+      }
+      else if (sum < minSum)
+      {
+         minSum = sum;
+         minNum = i;
+      }
+   }
+   Console.Write($"№ строки с мин суммой: {minNum + 1}");
+}
+
+int m = GetNumber("Введите количество строк");
+int n = GetNumber("Введите количество столбцов");
+
+int[,] matrix = GetMatrix(m, n);
+PrintMatrix(matrix);
+Console.WriteLine("");
+MinSum(matrix);
